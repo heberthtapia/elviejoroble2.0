ae9b2fa129170dd92793b1eb0f34f5d29035a39d
heberthtapia <ht.heberth@gmail.com>
2017-02-24 12:00:21 -0400

    probando chat
    Probando chat

--- index.php
+++ index.php
@@ -199,7 +199,11 @@
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-
+<script>
+  (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  })(window, document, '_gscq','script','//widgets.getsitecontrol.com/72399/script.js');
+</script>
 </body>
 </html>
 
